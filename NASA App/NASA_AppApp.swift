14c3e3bd55965d822bad8275258ee620c101b85a
patilvedant01 <vedantpatil324@gmail.com>
2025-12-16 19:38:57 +0530

    Initial Commit

---
+++ NASA App/NASA_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASA_AppApp.swift
+//  NASA App
+//
+//  Created by Vedant Patil on 16/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NASA_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
